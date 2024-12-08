70d790de1c301942567a32f6533bbb10641259fc
tauseef-ameen <tauseef.125002@gmail.com>
2024-12-08 16:58:48 +0100

    initial commit - fluentd.conf

---
+++ src/main/java/com/sidecar/k8s/side_car_container/SideCarContainerApplication.java
@@ -0,0 +1,13 @@
+package com.sidecar.k8s.side_car_container;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SideCarContainerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SideCarContainerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sidecar/k8s/side_car_container/SideCarContainerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sidecar.k8s.side_car_container;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SideCarContainerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
